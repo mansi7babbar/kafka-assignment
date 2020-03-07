bc4a9e5871a5ed08a32393a24f11869553ba6a33
mansi7babbar <mansi7babbar@gmail.com>
2020-03-07 10:32:42 +0530

    updated kafka assignment

--- src/main/scala/com/knoldus/Consumer.scala
+++ src/main/scala/com/knoldus/Consumer.scala
@@ -13,10 +13,10 @@ object Consumer extends App {
     val props = new Properties()
     props.put("bootstrap.servers", "localhost:9092")
     props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
-    props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+    props.put("value.deserializer", classOf[CustomDeserializer])
     props.put("auto.offset.reset", "latest")
     props.put("group.id", "consumer-group")
-    val consumer: KafkaConsumer[String, String] = new KafkaConsumer[String, String](props)
+    val consumer: KafkaConsumer[String, User] = new KafkaConsumer[String, User](props)
 
     consumer.subscribe(util.Arrays.asList(topic))
     while (true) {
--- src/main/scala/com/knoldus/Producer.scala
+++ src/main/scala/com/knoldus/Producer.scala
@@ -3,6 +3,7 @@ package com.knoldus
 import java.io.{File, PrintWriter}
 import java.util.Properties
 
+import net.liftweb.json.{DefaultFormats, parse}
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
 
 object Producer extends App {
@@ -11,15 +12,17 @@ object Producer extends App {
     val props = new Properties()
     props.put("bootstrap.servers", "localhost:9092")
     props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
-    props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
-    val producer = new KafkaProducer[String, String](props)
+    props.put("value.serializer", classOf[CustomSerializer])
+    val producer = new KafkaProducer[String, User](props)
     val message =
       """{
         "id": 1,
         "name": "mansi",
         "password": "mansibabbar"
     }"""
-    val record = new ProducerRecord[String, String](topic, "key", message)
+    implicit val formats: DefaultFormats.type = DefaultFormats
+    val user = parse(message).extract[User]
+    val record = new ProducerRecord[String, User](topic, "key", user)
     writeMessage(message)
     producer.send(record)
     producer.close()
